3d26b0d1b3285de6edae732f05f7f62db1d64cba
Brad Ito <brad@retina.ai>
2017-11-13 17:21:54 -0800

    adding perfect-paca boilerplate

---
+++ src-nuxt/pages/hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    The following loaded from api backend: <br />
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import axios_factory from '~/lib/axios_factory'
+
+export default {
+  head () {
+    return {
+      title: 'Hello World'
+    }
+  },
+  asyncData (context) {
+    const axios = axios_factory(context)
+
+    return axios.get('/api/hello-world')
+    .then((response) => {
+      return {
+        message: response.data.output
+      }
+    })
+  }
+}
+</script>
